479e4752c7101576dc2bbe28ed6cc21623b074aa
Gary A. Stafford <garystafford@rochester.rr.com>
2014-12-09 23:27:04 -0500

    Initial commit of project to repo

---
+++ README.markdown
@@ -0,0 +1,24 @@
+# fig #
+
+Puppet module for installing Fig (http://www.fig.sh).
+
+## Support
+
+This module is currently tested on:
+
+* Ubuntu 12.04
+* Ubuntu 14.04
+
+It may work on other distros...
+
+## Usage
+
+The module includes a single class:
+
+```puppet
+include 'fig'
+```
+
+```puppet
+class { 'fig': }
+```
---
+++ manifests/init.pp
@@ -0,0 +1,30 @@
+# Class: fig
+#
+# This module install Fig (http://www.fig.sh)
+#
+# Parameters: none
+#
+# Actions:
+#
+# Requires: see Modulefile
+#
+# Sample Usage: include fig
+#
+class fig {
+  exec { "install-fig":
+    path    => ["/usr/bin/", "/usr/sbin/", "/bin"],
+    command => "curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` > /tmp/fig",
+    user    => root,
+  } ~>
+  exec { "move-fig":
+    path    => ["/usr/bin/", "/usr/sbin/", "/bin"],
+    command => "mv /tmp/fig /usr/local/bin",
+    user    => root,
+  } ~>
+  file { "/usr/local/bin/fig":
+    path    => "/usr/local/bin/fig",
+    owner   => "root",
+    group   => "root",
+    mode    => "0755",
+  }
+}
