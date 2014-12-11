bb64cf9c692db13b161ca3b734642b054ebb56ca
Gary A. Stafford <garystafford@rochester.rr.com>
2014-12-10 20:17:11 -0500

    Fixing command chaining in init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,12 +15,12 @@ class fig {
     path    => ["/usr/bin/", "/usr/sbin/", "/bin"],
     command => "curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` > /tmp/fig",
     user    => root,
-  } ~>
+  } ->
   exec { "move-fig":
     path    => ["/usr/bin/", "/usr/sbin/", "/bin"],
     command => "mv /tmp/fig /usr/local/bin",
     user    => root,
-  } ~>
+  } ->
   file { "/usr/local/bin/fig":
     path    => "/usr/local/bin/fig",
     owner   => "root",
