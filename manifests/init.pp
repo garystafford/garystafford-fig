bf4439641c5dc22c7735625c3fa24daede1e46d7
Gary A. Stafford <garystafford@rochester.rr.com>
2014-12-10 21:02:28 -0500

    Fixing puppet-lint errors

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,20 +11,20 @@
 # Sample Usage: include fig
 #
 class fig {
-  exec { "install-fig":
-    path    => ["/usr/bin/", "/usr/sbin/", "/bin"],
-    command => "curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` > /tmp/fig",
+  exec { 'install-fig':
+    path    => ['/usr/bin/', '/usr/sbin/', '/bin'],
+    command => 'curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` > /tmp/fig',
     user    => root,
   } ->
-  exec { "move-fig":
-    path    => ["/usr/bin/", "/usr/sbin/", "/bin"],
-    command => "mv /tmp/fig /usr/local/bin",
+  exec { 'move-fig':
+    path    => ['/usr/bin/', '/usr/sbin/', '/bin'],
+    command => 'mv /tmp/fig /usr/local/bin',
     user    => root,
   } ->
-  file { "/usr/local/bin/fig":
-    path  => "/usr/local/bin/fig",
-    owner => "root",
-    group => "root",
-    mode  => "0755",
+  file { '/usr/local/bin/fig':
+    path  => '/usr/local/bin/fig',
+    owner => 'root',
+    group => 'root',
+    mode  => '0755',
   }
 }
